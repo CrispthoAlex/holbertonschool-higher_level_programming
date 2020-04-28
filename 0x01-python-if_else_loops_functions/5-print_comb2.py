97399bfda6202b7b4ecb396abf66b213a6e8d66c
crispthoalex <alex877@gmail.com>
2020-04-28 20:56:15 +0000

    Prints 00..99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i != 0:
-        print(", ", end="")
-    print("{:02d}".format(i), end="")
-print("")
+for i in range(0, 99):
+    print("{:02d}, ".format(i), end="")
+print("{:d}".format(99))
