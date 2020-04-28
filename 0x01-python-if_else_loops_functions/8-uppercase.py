9ce451325b8a4d7c6dee10f3255e4e7fa972b76e
crispthoalex <alex877@gmail.com>
2020-04-28 23:35:51 +0000

    Add is_upper function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 def uppercase(str):
     for i in str:
         if ord(i) > 96 and ord(i) < 123:
-            print("{:s}".format(chr(ord(i) - 32)), end="")
-        else:
-            print(i, end="")
+            i = chr(ord(i) - 32)
+        print(i, end="")
     print("")
