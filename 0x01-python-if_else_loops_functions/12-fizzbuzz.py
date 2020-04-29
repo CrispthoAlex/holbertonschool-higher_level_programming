086c620ea7dec6ab46c6d4e5c9afd6b0e574fa07
crispthoalex <alex877@gmail.com>
2020-04-29 02:15:51 +0000

    add fizzbuzz test improved

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("{:s} ".format("FizzBuzz"), end="")
-        elif i % 3 == 0:
-            print("{:s} ".format("Fizz"), end="")
-        elif i % 5 == 0:
-            print("{:s} ".format("Buzz"), end="")
-        else:
-            print("{:d} ".format(i), end="")
+        getout = ""
+        if i % 3 == 0:
+            getout += "Fizz"
+        if i % 5 == 0:
+            getout += "Buzz"
+        if getout == "":
+            getout = i
+        print(getout, end=" ")
