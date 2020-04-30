a3efb1f66ecbc29dc6e57dc1f197f85cc8db267d
crispthoalex <alex877@gmail.com>
2020-04-30 22:10:41 +0000

    add function sum for import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-def add(a, b):
-    a = 1
-    b = 2
-    if __name__ == "__main__":
-        print("{:d} + {:d} = {:d}".format(a, b, int(a + b)))
+a = 1
+b = 2
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, int(a + b)))
