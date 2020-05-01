3a45e5f73f73094fb290fe90c64769676c937fc9
crispthoalex <alex877@gmail.com>
2020-05-01 00:29:01 +0000

    Up Add infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    nuargv = len(sys.argv) # number of arguments
+    nuargv = len(sys.argv)  # number of arguments
     add = 0
     if nuargv > 1:
         for i in range(1, nuargv):
