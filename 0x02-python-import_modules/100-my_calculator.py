0adb45bfffe9a90f5074d0f5f7789a55138c119d
crispthoalex <alex877@gmail.com>
2020-05-01 04:47:32 +0000

    Add my first toolbox

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys  # to use argv
+    operator = '+-*/'
+    nuargv = len(sys.argv) - 1
+    errormsg = ['Usage: ./100-my_calculator.py <a> <operator> <b>',
+                'Unknown operator. Available operators: +, -, * and /']
+    if nuargv == 3:
+        if argv[2] in operator:
+            a = int(argv[1])
+            b = int(argv[3])
+            if sys.argv[2] == operator[0]:  # + add
+                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            if sys.argv[2] == operator[1]:  # - sub
+                print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+            if sys.argv[2] == operator[2]:  # * mul
+                print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+            if sys.argv[2] == operator[0]:  # / div
+                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        else:
+            print(errormsg[1])
+            exit(1)
+    else:
+        print(errormsg[0])
+        exit(1)
