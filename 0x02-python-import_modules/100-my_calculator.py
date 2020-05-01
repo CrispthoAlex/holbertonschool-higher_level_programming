d69a00a691e64c4c57ef3a76adc9c5e9830047a9
crispthoalex <alex877@gmail.com>
2020-05-01 04:50:33 +0000

    Add my first toolbox - fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     errormsg = ['Usage: ./100-my_calculator.py <a> <operator> <b>',
                 'Unknown operator. Available operators: +, -, * and /']
     if nuargv == 3:
-        if argv[2] in operator:
-            a = int(argv[1])
-            b = int(argv[3])
+        if sys.argv[2] in operator:
+            a = int(sys.argv[1])
+            b = int(sys.argv[3])
             if sys.argv[2] == operator[0]:  # + add
                 print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
             if sys.argv[2] == operator[1]:  # - sub
