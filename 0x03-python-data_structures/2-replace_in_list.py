496771f8fd7978b383902ee012aaf69b7fc2a21e
crispthoalex <alex877@gmail.com>
2020-05-05 19:09:47 +0000

    replaces an element of a list at a given index - fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    newlist = my_list.copy()
-    if idx > 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
+        newlist = my_list
         newlist[idx] = element
         return(newlist)
-    else:
-        return(my_list)
+    return(my_list)
