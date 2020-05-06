907285ffda31d76c73cc32f10bbb4afa9fa92d7e
crispthoalex <alex877@gmail.com>
2020-05-06 01:23:09 +0000

    adds 2 tuples fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,8 +4,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += (0, 0)
     if len(tuple_b) < 2 or tuple_b == ():
         tuple_b += (0, 0)
-    if tuple_a != () and tuple_b != ():
-        return(tuple(sum(i) for i in zip(tuple_a, tuple_b)))
+    newtuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (newtuple)
+
+#  return(tuple(sum(i) for i in zip(tuple_a, tuple_b))) <-- remember
 
 #  zip() https://realpython.com/python-zip-function/
 #  /understanding-the-python-zip-function
