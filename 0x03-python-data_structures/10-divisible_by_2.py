9c89b743626d182c94692f84dbee66315b11b3b5
crispthoalex <alex877@gmail.com>
2020-05-06 03:50:09 +0000

    finds all multiples of 2 in a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list != []:
+        div2 = [True, False]
+        listdiv = my_list.copy()
+        for i in range(len(my_list)):
+            if i % 2 == 0:
+                listdiv[i] = div2[0]
+            else:
+                listdiv[i] = div2[1]
+        return(listdiv)
+
+#  return(tuple(sum(i) for i in zip(tuple_a, tuple_b))) <-- remember
+
+#  zip() https://realpython.com/python-zip-function/
+#  /understanding-the-python-zip-function
