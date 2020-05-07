1c0409605c79eecb3fa4079170c81f73c13615a6
crispthoalex <alex877@gmail.com>
2020-05-07 17:40:16 +0000

    replace all occurrences fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,3 +4,4 @@ def search_replace(my_list, search, replace):
         new_list = my_list.copy()
         new_list[:] = [x if x != search else replace for x in new_list]
         return(new_list)
+    return (my_list)
