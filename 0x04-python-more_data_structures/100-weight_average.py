c048b9f0ede9fadce83b97934190b234e3f89088
crispthoalex <alex877@gmail.com>
2020-05-08 06:37:11 +0000

    reborn

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list:
-        sumdiv = sumprod = multiaxb = average = 0
+        sumdiv = sumprod = multiaxb = 0
         for tuple in my_list:
             sumdiv += tuple[1]
             multiaxb = tuple[0] * tuple[1]  # sum tuple index
             sumprod += multiaxb
-            average = sumprod / sumdiv
-        return average
+        return (sumprod / sumduv) # average
     return 0
