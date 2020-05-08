414c7a03eab4271d4fc91b6be2e5b3a943f12d2e
crispthoalex <alex877@gmail.com>
2020-05-08 05:59:21 +0000

    Add weight average of integrs tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0 or my_list is None:
-        return 0
-    suma = sumb = product = average = 0
-    for tuple in my_list:
-        suma += tuple[1]
-        product = tuple[0] * tuple[1]
-        sumb += product
-        average = sumb / suma
-    return average
+    if my_list:
+        sumdiv = sumprod = multiaxb = average = 0
+        for tuple in my_list:
+            sumdiv += tuple[1]
+            multiaxb = tuple[0] * tuple[1] #  sum tuple index
+            sumprod += multiaxb
+            average = sumprod / sumdiv
+        return average
+    return 0
