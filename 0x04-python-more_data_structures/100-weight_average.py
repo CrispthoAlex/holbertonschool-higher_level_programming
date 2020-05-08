8bf67a6b4c7e4c000002243e7ae6d0cf2f709c9f
crispthoalex <alex877@gmail.com>
2020-05-08 06:00:18 +0000

    Fix pep8

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
         sumdiv = sumprod = multiaxb = average = 0
         for tuple in my_list:
             sumdiv += tuple[1]
-            multiaxb = tuple[0] * tuple[1] #  sum tuple index
+            multiaxb = tuple[0] * tuple[1]  # sum tuple index
             sumprod += multiaxb
             average = sumprod / sumdiv
         return average
