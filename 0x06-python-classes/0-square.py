4b3aec5bf9a107890da9a7041bbbb54b25f00df4
crispthoalex <alex877@gmail.com>
2020-05-19 23:00:15 +0000

    defines a class square empty

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+My first square: Write an empty class Square that defines a square:
+
+* You are not allowed to import any module
+
+"""
+
+
+class Square:
+    """ class empty """
+    pass
