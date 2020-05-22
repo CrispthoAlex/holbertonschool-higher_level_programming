d03880a20db733a3830ee95f549dbc4f6d8ba866
crispthoalex <alex877@gmail.com>
2020-05-22 01:04:09 +0000

    fix wc -l docstrings

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
 Return addition of a and b
-
 >>> add_integer(1, 2)
 3
 """
 
 
 def add_integer(a, b=98):
-    """Return the factorial of n, an exact integer >= 0. Addition module
-
+    """Return the addition of a + b, an exact integer >= 0.
+       Addition module
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
