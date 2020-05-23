57cc203b1368a77118ea6f7c1cd0274cd55f2ddf
crispthoalex <alex877@gmail.com>
2020-05-23 05:31:08 +0000

    Add new cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,7 +17,46 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3]), 3)
         self.assertEqual(max_integer([-10, -2, -103]), -2)
         self.assertEqual(max_integer([-1.5, -5.2, 10.3]), 10.3)
+
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([45, 45, 45, 45]), 45)
-        self.assertEqual(max_integer([5.6, 34, 5, 45.8]), 45.8)
-        self.assertEqual(max_integer([80]), 80)
+
+    def setUp(self):
+        pass
+
+    def test_all_same(self):
+        self.assertEqual(max_integer([0, 0, 0]), 0)
+
+    def test_ordered_list(self):
+        ordered = [1, 2, 3, 4]
+        self.assertEqual(max_integer(ordered), 4)
+
+    def test_unordered_list(self):
+        unordered = [2, 3, 4, 1]
+        self.assertEqual(max_integer(unordered), 4)
+
+    def test_single_element_list(self):
+        single_element_list = [18]
+        self.assertEqual(max_integer(single_element_list), 18)
+
+    def test_negatives(self):
+        test_list = [-4, 0, 6, 4]
+        self.assertEqual(max_integer(test_list), 6)
+
+        test_list = [-7, -6, -5, -4]
+        self.assertEqual(max_integer(test_list), -4)
+
+    def test_max_integer(self):
+        test_list = [1, 2, 3, 9, 4]
+        self.assertEqual(max_integer(test_list), 9)
+
+    def test_max_integer_neg(self):
+        test_list = [1, 2, 3, 4, 5, -10, -100, -1000, 0]
+        self.assertEqual(max_integer(test_list), 5)
+
+    def test_max_integer_(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_positives(self):
+        test_list = [4, 2, 3, 1]
+        self.assertEqual(max_integer(test_list), 4)
