179af94d4cfe84014aae423d0fb78ff7edba620d
crispthoalex <alex877@gmail.com>
2020-05-26 04:22:23 +0000

    fix conditional isinstance private attributes

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ setter """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -72,7 +72,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """  setter """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
