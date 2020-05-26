1657744bb48e9dde0f65b0845a457fd5e24303c1
crispthoalex <alex877@gmail.com>
2020-05-26 04:18:22 +0000

    fix conditional rect_ >= rect_2

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -124,9 +124,9 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        elif rect_1.area() < rect_2.area():
-            return rect_2
-        return rect_1
+        elif rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
 
     @classmethod
     def square(cls, size=0):
