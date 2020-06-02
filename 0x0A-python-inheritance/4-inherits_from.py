fbdb3d3736145f5b459c60405c76bb20c1500649
crispthoalex <alex877@gmail.com>
2020-06-02 23:49:29 +0000

    fix docstring

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-def inherits_from(obj, a_class):
-    """
+"""
     function that returns True if the object is an instance of a
     class that inherited (directly or indirectly) from the specified
     class ; otherwise False
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Args:  @obj: object
+           @a_class: class
     """
     return isinstance(obj, a_class) and type(obj) != a_class
