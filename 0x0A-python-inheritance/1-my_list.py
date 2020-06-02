cc6236f0b8a5ed64b56045204b06ca8b8a15a9af
crispthoalex <alex877@gmail.com>
2020-06-02 22:41:22 +0000

    Add files

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3#!/usr/bin/python3
+"""
+class MyList from list:
+
+def print_sorted(self): that prints the list, but sorted (ascending sort)
+"""
+
+
+class MyList(list):
+    """
+    Args: list from main
+    """
+
+    def print_sorted(self):
+        """
+        Public instance method that prints the list, but
+        sorted (ascending sort)
+        Args: @self: inherits the input from self
+        """
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3#!/usr/bin/python3
+"""
+class BaseGeometry:
+Empty class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    pass
