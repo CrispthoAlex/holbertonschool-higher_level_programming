0abbe7e6904774ee685804285c6b203bc6976052
crispthoalex <alex877@gmail.com>
2020-06-03 03:57:53 +0000

    Add Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Class Rectangle """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square
+    """
+
+    def __init__(self, size):
+        """
+        * Instantiation size:
+            Validated by integer_validator
+        Args: @size: size
+        """
+        super().integer_validator("size", size)  # validator of class Rectangle
+        self.__size = size
+        super().__init__(self.__size, self.__size)  # init width and height
+
+    def area(self):
+        """
+        Area method
+        Args: @self: size
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        str module return a representation of class
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
