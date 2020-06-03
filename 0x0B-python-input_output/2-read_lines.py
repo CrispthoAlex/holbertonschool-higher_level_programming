a9ade4c3d6e88eaec643bf74e648f8e5e32caf9b
crispthoalex <alex877@gmail.com>
2020-06-03 22:11:15 +0000

    fix pep8: comments

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -9,8 +9,8 @@ def read_lines(filename="", nb_lines=0):
         @nb_lines: number of lines to read of file
     """
     with open(filename, mode="r", encoding="utf-8") as file:
-        list_lines = list(file) # list of lines of file
-        lines_cont = len(list_lines) # lenght of list of lines
+        list_lines = list(file)  # list of lines of file
+        lines_cont = len(list_lines)  # lenght of list of lines
 
         if nb_lines <= 0 or nb_lines > lines_cont:
             for line in list_lines:
