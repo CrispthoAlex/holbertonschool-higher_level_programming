2fbfeaaa660e1b0e5bb080aa21cff409c36595e6
crispthoalex <alex877@gmail.com>
2020-06-03 04:48:46 +0000

    Add My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Class Myint """
+
+
+class MyInt(int):
+    """
+    class MyInt that inherits from int
+    """
+
+    def __eq__(self, nb):
+        """
+        initialitation
+        """
+        return True if type(nb) is self.__class__ else False
+
+    def __ne__(self, nb):
+        """
+        initialitation
+        """
+        return True if type(nb) is not self.__class__ else False
