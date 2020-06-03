1f8df13d8c5211df03c6827780bc87daad488e18
crispthoalex <alex877@gmail.com>
2020-06-03 05:41:09 +0000

    add more docstring

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,6 +8,9 @@ def add_attribute(obje, key, value):
         @obje: Object
         @key: key
         @value: valuee
+    * hsattr: The arguments are an object and a string.
+      The result is True if the string is the name of one
+      of the objects attributes, False if not
     """
     if hasattr(obje, "__dict__"):
         setattr(obje, key, value)
