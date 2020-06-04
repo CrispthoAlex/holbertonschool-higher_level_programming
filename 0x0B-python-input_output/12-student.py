908c8230a547c83bada454e6da417b4cda6d029c
crispthoalex <alex877@gmail.com>
2020-06-04 03:57:14 +0000

    Add Student to JSON with filter

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+class Student that defines a student by:
+
+Public instance attributes:
+first_name
+last_name
+age
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation
+        of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        if all(attrs):
+            my_dic = {}
+            for elements in attrs:
+                if elements in self.__dict__ and type(elements) == str:
+                    my_dic[elements] = self.__dict__[elements]
+            return my_dic
