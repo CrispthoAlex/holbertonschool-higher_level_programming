d4410687a23cd9b1bcac896472ddb515dee16d52
crispthoalex <alex877@gmail.com>
2020-06-04 03:02:59 +0000

    Add Student to JSON

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+class Student that defines a student by:
+
+Public instance attributes:
+first_name
+last_name
+age
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation"""
+        return self.__dict__
