d4f3937639039d04f8dd180882f3458a48a968bd
crispthoalex <alex877@gmail.com>
2020-06-04 04:23:05 +0000

    fix pep8 - for...

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -32,5 +32,5 @@ class Student:
 
     def reload_from_json(self, json):
         """  replaces all attributes of the Student instance """
-        for e_key, e_value  in json.items():
+        for e_key, e_value in json.items():
             self.__dic__[e_key] = e_value
