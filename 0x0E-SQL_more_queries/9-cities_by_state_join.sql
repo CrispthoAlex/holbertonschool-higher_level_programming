946b61d03736838ef6c34481dea05bfb410403d2
crispthoalex <alex877@gmail.com>
2020-07-15 02:23:14 +0000

    Add ORBER BY cities.id

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,5 @@
 -- Each record should display: cities.id - cities.name - states.name
 SELECT cities.id, cities.name, states.name
 FROM cities LEFT JOIN states
-ON cities.state_id = states.id;
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
