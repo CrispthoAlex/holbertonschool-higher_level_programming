c2a313a3711daf25235e9915fffb28bc02b560f0
crispthoalex <alex877@gmail.com>
2020-08-18 23:59:16 +0000

    Add -w flag and improve code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument, and displays only the status code
-curl -sI "$1" | grep HTTP/1.0 | cut -d ' ' -f2
+curl -sw "%{http_code}" "$1" -o /dev/null
