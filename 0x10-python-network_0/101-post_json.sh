98554b3a33ebfbe1c0966b0050422f1f0e8cf931
crispthoalex <alex877@gmail.com>
2020-08-19 03:23:34 +0000

    Complete docstring

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that ends a JSON POST request to a URL passed as the first argument
+# Script that sends a JSON POST request to a URL passed as the first argument
 curl -s  "$1" -d @"$2" -H "Content-Type: application/json"
