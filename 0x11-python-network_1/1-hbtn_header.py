c6ffb0ff5ce19849e64e9f4c92e78a9803c256c4
crispthoalex <alex877@gmail.com>
2020-08-24 17:49:48 +0000

    Add import sys

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status  """
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable
+"""
 import urllib.request
+from sys import argv
 
 
 if __name__ == "__main__":
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(argv[1]) as response:
         header = response.headers.get('X-Request-Id')
 
     print(header)
