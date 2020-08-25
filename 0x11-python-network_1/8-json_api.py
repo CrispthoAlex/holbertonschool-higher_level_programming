ca156acb16d127fd64d484ecfb1124f06fb9c31f
crispthoalex <alex877@gmail.com>
2020-08-25 03:15:02 +0000

    Organized try-except

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,20 +11,21 @@ if __name__ == "__main__":
 
     url = 'http://0.0.0.0:5000/search_user'
 
-    try:
-        if argv[1] == "":
-            value = {'q': ""}
-        else:
-            value = {'q': argv[1]}
+    if len(argv) == 1:
+        value = {'q': ""}
+    else:
+        value = {'q': argv[1]}
 
-            req = requests.post(url, data=value)
-            req_json = req.json()
-            id_req = req_json.get('id')
-            name_req = req_json.get('name')
+    req = requests.post(url, data=value)
+
+    try:
+        req_json = req.json()
 
         if req_json is {} or (id_req is None or name_req is None):
             print("No result")
         else:
+            id_req = req_json.get('id')
+            name_req = req_json.get('name')
             print("[{}] {}".format(id_req, name_req))
 
     except Exception:
