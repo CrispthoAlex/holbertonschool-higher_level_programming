2b169bf7f04f2739d932c639eef3a5f23fabee11
crispthoalex <alex877@gmail.com>
2020-08-25 00:35:51 +0000

    Add script that sends a request to the URL and manage error

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays the body
+of the response (decoded in utf-8)
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    # values = {'email': argv[2]}  # dictionary to request
+    # data = urllib.parse.urlencode(values).encode('ascii')  data shoulb be bytes
+    req = requests.get(url)
+
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
