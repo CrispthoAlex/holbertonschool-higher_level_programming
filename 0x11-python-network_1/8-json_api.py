345b4a8e4ead800ada6678c44bc75fc81e6b6e85
crispthoalex <alex877@gmail.com>
2020-08-25 02:54:20 +0000

    Add script sends a POST request to a url

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+   url = 'http://0.0.0.0:5000/search_user'
+
+   try:
+      if argv[1] == "":
+         value = {'q': ""}
+      else:
+         value = {'q': argv[1]}
+
+      req = requests.post(url, data=value)
+      req_json = req.json()
+      id_req = req_json.get('id')
+      name_req = req_json.get('name')
+
+      if req_json is {} or (id_req == None or name_req == None):
+         print("No result")
+      else:
+         print("[{}] {}".format(id_req, name_req))
+
+   except Exception:
+      print("Not a valid JSON")
