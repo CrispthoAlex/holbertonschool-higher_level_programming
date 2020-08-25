f78e24f368ddcb557181c890b2a4528290834afc
crispthoalex <alex877@gmail.com>
2020-08-25 03:33:04 +0000

    Add script that takes your Github credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that takes your Github credentials
+(username = argv[1], passwd = argv[2])
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    url = 'https://api.github.com/user'
+    req = requests.get(url, auth=(argv[1], argv[2]))
+    req_json = req.json()
+    print(req_json.get('id'))
