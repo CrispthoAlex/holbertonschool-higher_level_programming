22fcbc45b277dc292088d637ffe53129e855b638
crispthoalex <alex877@gmail.com>
2020-08-25 02:59:48 +0000

    Fix tabulation

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,23 +9,23 @@ from sys import argv
 
 if __name__ == "__main__":
 
-   url = 'http://0.0.0.0:5000/search_user'
+    url = 'http://0.0.0.0:5000/search_user'
 
-   try:
-      if argv[1] == "":
-         value = {'q': ""}
-      else:
-         value = {'q': argv[1]}
+    try:
+        if argv[1] == "":
+            value = {'q': ""}
+        else:
+            value = {'q': argv[1]}
 
-      req = requests.post(url, data=value)
-      req_json = req.json()
-      id_req = req_json.get('id')
-      name_req = req_json.get('name')
+            req = requests.post(url, data=value)
+            req_json = req.json()
+            id_req = req_json.get('id')
+            name_req = req_json.get('name')
 
-      if req_json is {} or (id_req == None or name_req == None):
-         print("No result")
-      else:
-         print("[{}] {}".format(id_req, name_req))
+        if req_json is {} or (id_req is None or name_req is None):
+            print("No result")
+        else:
+            print("[{}] {}".format(id_req, name_req))
 
-   except Exception:
-      print("Not a valid JSON")
+    except Exception:
+        print("Not a valid JSON")
