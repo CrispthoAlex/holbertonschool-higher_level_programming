2e926aa0fa812b8f8b49ecffc4509eeb9259b714
crispthoalex <alex877@gmail.com>
2020-08-25 05:40:41 +0000

    Fix conditional

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,19 +12,19 @@ if __name__ == "__main__":
     repo = argv[1]
     user = argv[2]
     # example
-    # https://api.github.com/repos/octocat/Hello-World/git/commits/
-    url = 'https://api.github.com/repos/{}/{}/git/commits/'.format(
+    # GET /repos/:owner/:repo/commits
+    url = 'https://api.github.com/repos/{}/{}/commits/'.format(
         user, repo)
 
     try:
         req = requests.get(url)
         req_json = req.json()
-
-        if req_json:
+        print(req_json)
+        if req_json and req_json.get('message') is not 'Not Found':
             count = 0
             for commit in req_json:
                 count += 1
-                if count == 10:
+                if count <= 11:
                     break
                 sha_repo = commit.get('sha')
                 user_name = commit.get('author').get('name')
