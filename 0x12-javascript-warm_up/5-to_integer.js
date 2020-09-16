1248d0f6e21f3b22e7273115fa0f47b324ab22fe
crispthoalex <alex877@gmail.com>
2020-09-16 03:00:58 +0000

    Add Script that prints a string if the first argument is integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that prints a string if the first argument is integer
+// process.argv
+// console.log(`${process.argv}`)
+const num = parseInt(process.argv[2])
+if (num) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log('Not a number');
+}
