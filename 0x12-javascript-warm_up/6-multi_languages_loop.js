1ba4360b764da0c18719fc6edbc55ded2787300c
crispthoalex <alex877@gmail.com>
2020-09-16 03:34:47 +0000

    Fix guide style and structure

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // Script that prints 3 lines using array and loop
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-myVar.forEach((txt) => {
-  console.log(`${txt}`);
-});
+const text = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let txt;
+for (txt of text) console.log(txt);
