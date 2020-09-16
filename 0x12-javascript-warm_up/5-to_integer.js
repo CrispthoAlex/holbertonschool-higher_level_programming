8e31f52091bb2fbd32e408ff4d2e33a7b7e7167f
crispthoalex <alex877@gmail.com>
2020-09-16 03:02:21 +0000

    Fix guide style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // Script that prints a string if the first argument is integer
 // process.argv
 // console.log(`${process.argv}`)
-const num = parseInt(process.argv[2])
+const num = parseInt(process.argv[2]);
 if (num) {
   console.log(`My number: ${num}`);
 } else {
