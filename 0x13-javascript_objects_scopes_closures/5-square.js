48ed0fefa400a85584f4f41e26c2056f560f489b
crispthoalex <alex877@gmail.com>
2020-09-21 23:44:24 +0000

    Remove additional methods

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -7,19 +7,4 @@ module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
-
-  print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
-  }
-
-  rotate () {
-    // const temp = this.width;
-    // Swap variables: https://javascript.info/destructuring-assignment
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
 };
