3dd59adca6b2e42a4e943e3f6ab00300ed68d140
crispthoalex <alex877@gmail.com>
2020-09-21 21:08:42 +0000

    Add class Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Write an empty class Rectangle that defines a rectangle
+// set the exports object to a function or a new object
+module.exports = class Rectangle {
+//  constructor () {}
+};
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write an empty class Rectangle that defines a rectangle
+// set the exports object to a function or a new object
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
