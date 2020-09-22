fe45ee25249563e7d899ecfdf5a4744203150ff8
crispthoalex <alex877@gmail.com>
2020-09-22 06:48:46 +0000

    Fix guide style

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Write a script that concats 2 files
-const fileA = process.argv[2]
-const fileB = process.argv[3]
+const fileA = process.argv[2];
+const fileB = process.argv[3];
 const fileC = process.argv[4];
 const fs = require('fs');
 
