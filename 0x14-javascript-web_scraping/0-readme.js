0ddcfd89a5a1343877b3316ea7c4d1d1032cf6c6
crispthoalex <alex877@gmail.com>
2020-09-22 17:48:51 +0000

    Add script that prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file
+const file = process.argv[2];
+const fs = require('fs');
+
+if (file) {
+  // Asynchronous version of fs.readFile. Returns the contents of the filename
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err)
+    } else console.log(data);
+  });
+}
