af548def5a3bf9d23d3adb085dd694171b76389d
crispthoalex <alex877@gmail.com>
2020-09-23 08:29:55 +0000

    Improving output of script In this script was applied: * Promise() method * async keyword: ensures that the function returns a promise, e.g.:       async function f() {       	return 1;       } * await keyword: works only inside async functions. e.g.:       async function f() {
      	let promise = new Promise((resolve, reject) => {
        	  setTimeout(() => resolve("done!"), 1000)
     	});
    
    	let result = await promise; // wait until the promise resolves (*)
    	}

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,29 +1,28 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars movie
+// in the same order of the list characters in the /films/ response
 const movId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movId;
 const request = require('request');
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const list = getName(body);
-    for (const elem of list) {
-      request(elem, (error, response, body1) => {
-        if (error) { console.log(error); } else { console.log(JSON.parse(body1).name); }
-      });
+// https://javascript.info/async-await
+request(url, async (error, response, body) => {
+  if (error) console.log(error);
+  else {
+    const listChar = JSON.parse(body).characters;
+    for (const elem of listChar) {
+      console.log(await getName(elem));
     }
   }
 });
 
-function getName (body) {
-  const listChar = JSON.parse(body).characters;
-  const returnList = [];
-  for (const apiPerson of listChar) {
-    request(apiPerson, (error, response, body1) => {
-      if (error) { console.log(error); } else { returnList.push(JSON.parse(body1).name); }
+// https://javascript.info/promise-basics
+function getName (apiPerson) {
+  const promise = new Promise((resolve, reject) => {
+    request(apiPerson, (error, response, body) => {
+      if (error) reject(error);
+      else resolve(JSON.parse(body).name);
     });
-  }
-  return listChar;
+  });
+  return promise;
 }
