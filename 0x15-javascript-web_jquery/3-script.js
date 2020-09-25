81699655c47b52b65097b660573d35a7e327e33f
crispthoalex <alex877@gmail.com>
2020-09-25 03:53:08 +0000

    Add Script that adds the class red

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+// Javascript script that adds the class red to the HTML tag HEADER to
+// red (#FF0000) when the user clicks on the tag DIV#red_header
+$('DIV#red_header').click(() => {
+  $('HEADER').addClass('red');
+});
